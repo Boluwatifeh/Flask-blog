bb6e59cdd972ef8e5e1c1c49d54009f776511dd1
boluwatifeh <tifeasypeasy@gmail.com>
2021-11-19 01:31:54 +0100

    testing render_template with dummy data

--- app.py
+++ app.py
@@ -2,15 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+
+    {
+        'author': 'Ogundeyi Boluwatife',
+        'title': 'Blog Post One',
+        'content': 'Testing out the posts functionality',
+        'date': 'November 19, 2020'
+    },
+
+
+    {
+        'author': 'Olasunkanmi Jesuferanmi',
+        'title': 'Blog Post Two',
+        'content': 'Testing out the posts functionality',
+        'date': 'November 19, 2020'
+    },
+
+     
+     {
+        'author': 'Oladoke Eniola',
+        'title': 'Blog Post Three',
+        'content': 'Testing out the posts functionality',
+        'date': 'November 19, 2020'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run()
